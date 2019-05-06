9853f945c85999a6cd070f79b327faa0d6e1af18
Andrew Young <andrew@youngshome.com>
2019-05-06 18:54:29 +0800

    Front-End Startup

--- public/script/program.js
+++ public/script/program.js
@@ -1,38 +1,3 @@
-class BlockPosition {
-    constructor(inputRow, inputCol)
-    {
-        this.row = inputRow;
-        this.col = inputCol;
-    }
-
-    getRow()
-    {
-        return this.row;
-    }
-
-    getCol()
-    {
-        return this.col;
-    }
-
-    getId()
-    {
-        var string = "";
-        if (this.row < 10)
-        {
-            string += "0";
-        }
-        string += this.row;
-        string += "-";
-        if (this.col < 10)
-        {
-            string += "0";
-        }
-        string += this.col;
-        return string;
-    }
-}
-
 var app = new Vue({
     el: '#app',
     data:
@@ -41,7 +6,37 @@ var app = new Vue({
     },
     methods:
     {
-        
+        async sendMove() {
+            let payload = {key: "r"};
+            let res = axios.put('/wanderer/', payload);
+            this.updateBoard(res.data);
+        },
+        updateBoard(data)
+        {
+            for (var i = 0; i < data.updates.length; i++)
+            {
+                for (var j = 0; j < data.updates[i].length; j++)
+                {
+                    var id = this.getId(data.updates[i][j].row, data.updates[i][j].col);
+                    var element = document.getElementById(id);
+                    if (data.updates[i][j].add != "")
+                    {
+                        element.classList.add(data.updates[i][j].add);
+                    }
+                    if (data.updates[i][j].sub != "")
+                    {
+                        if (element.classList.contains(data.updates[i][j].sub))
+                        {
+                            element.classList.remove(data.updates[i][j].sub);
+                        }
+                    }
+                }
+            }
+        },
+        getId(row, col)
+        {
+
+        }
     },
     computed:
     {
@@ -49,6 +44,6 @@ var app = new Vue({
     },
     created()
     {
-
+        let res = axios.post('/wanderer/');
     }
 });
---
+++ template.js
@@ -0,0 +1,18 @@
+// Request
+let req = 
+{
+    playerMove: "",
+}
+
+
+//Response
+let res = 
+{
+    updates: [ // List of Refreshed Screens
+        [ // First screen
+            {row: 0, col: 0, add: "", sub: ""}, // List of Individual Updates
+            {row: 0, col: 0, add: "", sub: ""}
+        ]
+    ],
+    score: 10,
+}
