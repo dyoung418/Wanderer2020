335d10651e344ff2db8396beca3980391bfa80cb
Andrew Young <andrew@youngshome.com>
2019-05-07 19:42:15 +0800

    Creating Level Data

---
+++ public/script/leveldata.js
@@ -0,0 +1,313 @@
+let LEVELS = {
+    level1: {
+        time: 1000,
+        author: "Steven Shipway",
+        title: "Unplug the Cistern #1",
+    },
+    level2: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level3: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level4: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level5: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level6: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level7: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level8: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level9: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level10: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level11: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level12: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level13: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level14: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level15: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level16: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level17: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level18: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level19: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level20: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level21: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level22: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level23: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level24: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level25: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level26: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level27: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level28: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level29: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level30: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level31: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level32: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level33: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level34: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level35: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level36: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level37: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level38: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level39: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level40: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level41: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level42: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level43: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level44: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level45: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level46: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level47: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level48: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level49: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level50: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level51: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level52: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level53: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level54: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level55: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level56: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level57: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level58: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level59: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level60: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level61: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+    level62: {
+        time: 1,
+        author: "Unknown",
+        title: "Unknown",
+    },
+};
+
